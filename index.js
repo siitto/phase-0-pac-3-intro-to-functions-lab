759fd7c358688ffc380ee502e6df3beb1eea7fbc
+ <+>
2023-09-28 21:34:00 -0400

    added functions to index.js

---
+++ index.js
@@ -0,0 +1,53 @@
+function shout(string){
+return string.toUpperCase();
+}
+
+function whisper(string){
+return string.toLowerCase();
+}
+
+function logShout(string){
+console.log(string.toUpperCase());
+}
+
+function logWhisper(string){
+console.log(string.toLowerCase());
+}
+
+const string2 = "hello";
+const string1 = "I can\'t hear you!"
+const string3 = "YES INDEED!";
+
+function sayHiToHeadphonedRoommate(string){
+
+if (string.toLowerCase()) {
+{
+    return string1;
+      }
+   console.log(string1);
+}
+Else
+ string === string.toUpperCase();    
+ if(string.toUpperCase()){
+      return string3;
+      }
+   console.log(string3);
+Else  
+
+const string5 = "Let's have dinner together!";   
+  string = string5
+
+const string6 ="I would love to!"
+ 
+
+if (string=="Let's have dinner together!") {
+    return string6;
+      }
+   console.log(string6);
+
+Else  
+   string = string6;
+return string;
+}
+console.log(string);
+
